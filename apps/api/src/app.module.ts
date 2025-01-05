2f1d23b34fa2347dd4b2d22e65a07af4c535efb7
Anjan KC <kcanjan2020@gmail.com>
2025-01-05 19:27:34 +0545

    feat: docker compose

--- apps/api/src/app.module.ts
+++ apps/api/src/app.module.ts
@@ -1,9 +1,12 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { ProductModule } from './product/product.module';
+import { OrderItemModule } from './order-item/order-item.module';
+import { PaymentMethodModule } from './payment-method/payment-method.module';
 
 @Module({
-  imports: [],
+  imports: [ProductModule, OrderItemModule, PaymentMethodModule],
   controllers: [AppController],
   providers: [AppService],
 })
