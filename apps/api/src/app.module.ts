8c086d656b539ef2f9f7d04b10c4373fa0ef7030
Anjan KC <kcanjan2020@gmail.com>
2025-01-05 20:52:52 +0545

    feat: Setup TypeOrm with PostgreSql

--- apps/api/src/app.module.ts
+++ apps/api/src/app.module.ts
@@ -4,9 +4,36 @@ import { AppService } from './app.service';
 import { ProductModule } from './product/product.module';
 import { OrderItemModule } from './order-item/order-item.module';
 import { PaymentMethodModule } from './payment-method/payment-method.module';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { Type } from 'class-transformer';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
-  imports: [ProductModule, OrderItemModule, PaymentMethodModule],
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+      envFilePath: '.env',
+      ignoreEnvFile: process.env.NODE_ENV === 'production',
+    }),
+    TypeOrmModule.forRootAsync({
+      imports: [ConfigModule],
+      useFactory: (confingService: ConfigService) => {
+        return {
+          type: 'postgres',
+          host: confingService.get('DB_HOST'),
+          port: confingService.get('DB_PORT'),
+          username: confingService.get('DB_USERNAME'),
+          password: confingService.get('DB_PASSWORD'),
+          database: confingService.get('DB_DATABASE'),
+          autoLoadEntities: true,
+        };
+      },
+      inject: [ConfigService],
+    }),
+    ProductModule,
+    OrderItemModule,
+    PaymentMethodModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
